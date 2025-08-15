a9f20f2b5e7443b32ef5a997ebd793c84ffdd720
Sehajdeep Singh <sehajdeepsdsn@gmail.com>
2025-08-15 23:47:49 +0530

    Create Q1.cpp

---
+++ Assignment 1/Q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World using \\n\n";
+    cout << "Hello World using endl" << endl;
+    cout << "Hello\tWorld with tab" << endl;
+    cout << "Hello World with alarm\a" << endl;
+    cout << "12345\rHello" << endl;
+    return 0;
+}
